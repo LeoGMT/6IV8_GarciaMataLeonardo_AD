0f0b35fe200ed84422707d1a4641087a694b1407
LeoGMT <garcia.mata.leonardo1@gmail.com>
2025-03-10 15:47:50 -0600

    Update 02Diccionario.py
    diccionario

--- ElementosBasicosEstadistica/02Diccionario.py
+++ ElementosBasicosEstadistica/02Diccionario.py
@@ -7,7 +7,12 @@ def estadistica_notas(notas):
     estadisticas= pd.Series([notas.min(), notas.max(), notas.mean(), notas.std()], index=['Min', 'Max', 'Media', 'Desviacion'])
     return estadisticas
 
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas>=6].sort_values(ascending=False)
+
 ## Corchetes para listas y llaves para lo que define los datos
-notas={'Juan': 9, 'Juanita': 7, 'pedro': 6.6, 'Fabian': 8.5, 'Maximiliano': 7.5, 'Sandra': 9.8, 'Rosario:'9}
+notas={'Juan': 9, 'Juanita': 7, 'pedro': 6.6, 'Fabian': 8.5, 'Maximiliano': 7.5, 'Sandra': 9.8, 'Rosario':9}
 
 print(estadistica_notas(notas))
+print(aprobados(notas))
